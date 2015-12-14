4921fd5a0f0bfe81e1cdbf097f38eff630aec99c
PizzaCrust <torchprojectserver@gmail.com>
2015-12-14 20:47:00 +0700

    Setup project.

---
+++ src/main/java/org/thallium/Main.java
@@ -0,0 +1,12 @@
+package org.thallium;
+
+import net.minecraft.server.MinecraftServer;
+
+/**
+ * Launches the minecraft server
+ */
+public class Main {
+    public static void main(String[] args){
+        MinecraftServer.main(args);
+    }
+}
